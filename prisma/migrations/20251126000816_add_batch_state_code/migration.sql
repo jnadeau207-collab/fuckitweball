5964d2f753564d274d6fc14b1d0257bc6e803297
jnadeau207-collab <jnadeau207@gmail.com>
2025-11-28 14:54:05 -0500

    Delete prisma directory

--- prisma/migrations/20251126000816_add_batch_state_code/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Batch" ADD COLUMN     "stateCode" TEXT;
--- prisma/migrations/20251126182828_lab_state_and_batch_jurisdiction/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "Batch" ADD COLUMN     "jurisdiction" TEXT;
-
--- AlterTable
-ALTER TABLE "Lab" ADD COLUMN     "city" TEXT,
-ADD COLUMN     "stateCode" TEXT;
