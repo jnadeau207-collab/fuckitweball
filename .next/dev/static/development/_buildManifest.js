60a29f58171f2dc8210c67437e2b23d78048ae07
jnadeau207-collab <jnadeau207@gmail.com>
2025-11-28 00:49:50 -0500

    working commit

--- .next/dev/static/development/_buildManifest.js
+++ .next/dev/static/development/_buildManifest.js
@@ -2,9 +2,6 @@ self.__BUILD_MANIFEST = {
   "/": [
     "static/chunks/pages/index.js"
   ],
-  "/admin": [
-    "static/chunks/pages/admin.js"
-  ],
   "__rewrites": {
     "afterFiles": [],
     "beforeFiles": [],
